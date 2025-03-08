07fa8a6af279de19db85cc4ec30272065ad5229f
Henrique Albuquerque <henriquealbuquerqueb6@gmail.com>
2025-03-08 14:45:43 -0300

    fix: inexistent component

--- app/components/circle/About.tsx
+++ app/components/circle/About.tsx
@@ -1,8 +1,6 @@
-import Circle from "../layout/circle/Circle";
-
 export function About() {
   return (
-    <Circle>
+    <div>
       <h1 className="font-bold text-2xl pb-4">Welcome to the About Page</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
@@ -16,6 +14,6 @@ export function About() {
         obcaecati voluptatem quasi corporis totam molestias aliquid, omnis
         dolores, ut minima et!
       </p>
-    </Circle>
+    </div>
   );
 }
